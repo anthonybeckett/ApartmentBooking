8111a2875ca17b7e8787c5c73aa11ce2f183d5a9
Anthony Beckett <antneyb10@gmail.com>
2025-01-03 22:52:24 +0000

    Added configuration files for generating the tables in the database

---
+++ ApartmentBooking.Infrastructure/Configurations/ApartmentConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using ApartmentBooking.Domain.Apartments;
+using ApartmentBooking.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApartmentBooking.Infrastructure.Configurations;
+
+internal class ApartmentConfiguration : IEntityTypeConfiguration<Apartment>
+{
+    public void Configure(EntityTypeBuilder<Apartment> builder)
+    {
+        builder.ToTable("apartments");
+
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(x => x.Address);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(200)
+            .HasConversion(name => name.Value, value => new Name(value));
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(2000)
+            .HasConversion(description => description.Value, value => new Description(value));
+
+        builder.OwnsOne(x => x.Price, priceBuilder => {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(x => x.CleaningFee, priceBuilder => {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+    }
+}
---
+++ ApartmentBooking.Infrastructure/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using ApartmentBooking.Domain.Apartments;
+using ApartmentBooking.Domain.Bookings;
+using ApartmentBooking.Domain.Users;
+using Bookify.Domain.Reviews;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using ApartmentBooking.Domain.Reviews;
+
+namespace ApartmentBooking.Infrastructure.Configurations;
+
+internal sealed class ReviewConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable("reviews");
+
+        builder.HasKey(review => review.Id);
+
+        builder.Property(review => review.Rating)
+            .HasConversion(rating => rating.Value, value => Rating.Create(value).Value);
+
+        builder.Property(review => review.Comment)
+            .HasMaxLength(200)
+            .HasConversion(comment => comment.Value, value => new Comment(value));
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(review => review.ApartmentId);
+
+        builder.HasOne<Booking>()
+            .WithMany()
+            .HasForeignKey(review => review.BookingId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(review => review.UserId);
+    }
+}
